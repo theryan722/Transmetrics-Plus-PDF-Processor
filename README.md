6118a4909d26b9685ab0f6c44888f1a3adcab4a5
Ryan O'Day <romanguy722@gmail.com>
2020-07-20 13:35:11 -0400

    PDF Test

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Transmetrics-Plus-PDF-Processor
  
+# Starting server
+
+First stop apache:
+sudo /opt/bitnami/ctlscript.sh stop apache
+
+Then run:
+sudo node index
+
+Will run on port 80
--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const port = 3000;
+const port = 80;
+const pdftk = require('node-pdftk');
 
 const cors = require('cors')({
     origin: true
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/pdf', function (req, res) {
+    pdftk
+        .input('./test.pdf')
+        .fillForm({
+            "1-AV#*": 'my av id!!'
+        })
+        .flatten()
+        .output()
+        .then(buffer => {
+            console.log('buf: ', buffer);
+            fs.writeFile('./written.pdf', buffer, function (err) {
+                if (err) return console.log(err);
+                res.sendFile('./written.pdf');
+            });
+        })
+        .catch(err => {
+            console.log('err: ', err);
+        });
+
+    
+
+});
 
 app.listen(port, () => console.log(`Example app listening at ${port}`));
